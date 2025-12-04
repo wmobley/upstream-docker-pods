1bed55b9a1db4dea36ea4d35e6065e6803e48494
Will Mobley <feulmob+github@gmail.com>
2025-12-04 15:52:17 -0600

    removed projects

--- app/api/v1/main.py
+++ app/api/v1/main.py
@@ -15,7 +15,6 @@ from app.api.v1.routes.sensor_variables.sensor_variables import (
 from app.api.v1.routes.campaigns.root import router as campaigns_router
 from app.api.v1.routes.root import router as root_router
 from app.api.v1.routes.upload_file.upload_csv import router as upload_file_csv_router # type: ignore[attr-defined]
-from app.api.v1.routes.projects.projects import router as projects_router
 from app.api.v1.routes.ckan import router as ckan_router
 from app.api.v1.routes.pods import router as pods_router
 from app.api.v1.routes.user_roles import router as user_roles_router
@@ -28,7 +27,6 @@ api_router.include_router(campaign_station_sensors_router)
 api_router.include_router(campaign_station_sensor_measurements_router)
 api_router.include_router(sensor_variables_router)
 api_router.include_router(upload_file_csv_router)
-api_router.include_router(projects_router)
 api_router.include_router(ckan_router)
 api_router.include_router(pods_router)
 api_router.include_router(user_roles_router)
